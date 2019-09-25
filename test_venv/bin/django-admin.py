020be39e50c4704473557be8c45077ef0ba6bd7d
omotosho <omotosho@viableknowledgemasters.com>
2019-09-25 23:09:47 +0100

    12

---
+++ test_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/omotosho/Desktop/VKM/test_cd_ci/django_azure/test_venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
