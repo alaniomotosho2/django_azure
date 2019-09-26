7d0a38b8ef8c4abb3f022e8c0a04e18f44965c8e
omotosho <omotosho@viableknowledgemasters.com>
2019-09-26 10:31:58 +0100

    tester

--- app/tests/test_urls.py
+++ app/tests/test_urls.py
@@ -7,12 +7,10 @@ import app
 
 class TestUrls(SimpleTestCase):
 	def test_index_url_resolves(self):
-		#assert 1 == 2
 		url = reverse('index')
 		self.assertEquals(resolve(url).func,index)
 
 
 	def test_current_datetime_url_resolves(self):
-		#assert 1 == 2
 		url = reverse('current_datetime')
 		self.assertEquals(resolve(url).func,current_datetime)
